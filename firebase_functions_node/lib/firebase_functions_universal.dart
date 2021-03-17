0571f123d513289f0b68a2d5fc5a1ecdcd94d449
alex <alex@tekartik.com>
2021-03-18 00:52:53 +0100

    remove node

--- firebase_functions_node/lib/firebase_functions_universal.dart
+++
@@ -1,4 +0,0 @@
-export 'package:tekartik_firebase_functions/firebase_functions.dart';
-
-export 'src/ff_universal_common.dart' show FirebaseFunctionsHttpUniversal;
-export 'src/universal/ff_universal.dart';
