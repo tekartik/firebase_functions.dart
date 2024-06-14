d09de7060f246229abe21913575bc91806307744
Alexandre Roux <alex@tekartik.com>
2024-06-14 17:52:04 +0200

    fix ci

--- firebase_functions/test/mock_test.dart
+++ firebase_functions/test/mock_test.dart
@@ -1,4 +1,5 @@
 import 'package:tekartik_firebase_functions/firebase_functions.dart';
+import 'package:test/test.dart';
 
 class SchedulerFunctionsMock
     with SchedulerFunctionsDefaultMixin
@@ -7,3 +8,10 @@ class SchedulerFunctionsMock
 class ScheduleEventMock
     with SchedulerEventDefaultMixin
     implements ScheduleEvent {}
+
+void main() {
+  test('mock', () {
+    ScheduleEventMock();
+    SchedulerFunctionsMock();
+  });
+}
