86e95471c126ee9ec3c760f3d2842b7d32a7038e
Alexandre Roux <alex@tekartik.com>
2026-05-31 19:16:08 +0200

    chore: update analysis options and suppress print warnings in Firebase functions

--- firebase_functions_http/lib/src/mixin.dart
+++ firebase_functions_http/lib/src/mixin.dart
@@ -1,5 +1,6 @@
 import '../firebase_functions_http.dart';
 
+/// Header key for passing user ID in call requests.
 final firebaseFunctionsHttpHeaderUid = 'x-tekartik-firebase-call-uid';
 
 /// Mixin helper
